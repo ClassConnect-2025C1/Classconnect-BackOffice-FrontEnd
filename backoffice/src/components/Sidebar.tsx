6aa803e4ad5ad4a92e441b3f3851a660a3247781
franciscoL0pez <franlopez.2001@hotmail.com>
2025-05-25 22:15:09 -0300

    Add icons

--- backoffice/src/components/Sidebar.tsx
+++ backoffice/src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
-// components/Sidebar.tsx
-import { Box, VStack, Button, Image, Text } from "@chakra-ui/react";
+import { Box, VStack, Button, Image, Text, HStack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { MdHome, MdPeople, MdPersonAdd, MdBarChart } from "react-icons/md";
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -16,31 +16,56 @@ const Sidebar = () => {
       position="fixed"
     >
       {/* Logo */}
-   <Box mb={10} textAlign="center">
-  <Image
-    src={logo}
-    alt="Logo"
-    boxSize="80px"
-    mx="auto"
-    mb={2}
-  />
-  <Text fontSize="lg" fontWeight="bold">
-    Admin Panel
-  </Text>
-</Box>
+      <Box mb={10} textAlign="center">
+        <Image src={logo} alt="Logo" boxSize="80px" mx="auto" mb={2} />
+        <Text fontSize="lg" fontWeight="bold">
+          Admin Panel
+        </Text>
+      </Box>
 
       {/* Navegación */}
       <VStack spacing={4} align="stretch">
-        <Button variant="ghost" color="white" _hover={{ bg: "#276749" }} onClick={() => navigate("/")}>
+        <Button
+          variant="ghost"
+          color="white"
+          _hover={{ bg: "#276749" }}
+          justifyContent="flex-start"
+          leftIcon={<MdHome size="20px" />}
+          onClick={() => navigate("/")}
+        >
           Home
         </Button>
-        <Button variant="ghost" color="white" _hover={{ bg: "#276749" }} onClick={() => navigate("/user-admin")}>
+
+        <Button
+          variant="ghost"
+          color="white"
+          _hover={{ bg: "#276749" }}
+          justifyContent="flex-start"
+          leftIcon={<MdPeople size="20px" />}
+          onClick={() => navigate("/user-admin")}
+        >
           Users
         </Button>
-        <Button variant="ghost" color="white" _hover={{ bg: "#276749" }} onClick={() => navigate("/create-admin")}>
+
+        <Button
+          variant="ghost"
+          color="white"
+          _hover={{ bg: "#276749" }}
+          justifyContent="flex-start"
+          leftIcon={<MdPersonAdd size="20px" />}
+          onClick={() => navigate("/create-admin")}
+        >
           Create Admin
         </Button>
-        <Button variant="ghost" color="white" _hover={{ bg: "#276749" }} onClick={() => navigate("/metrics")}>
+
+        <Button
+          variant="ghost"
+          color="white"
+          _hover={{ bg: "#276749" }}
+          justifyContent="flex-start"
+          leftIcon={<MdBarChart size="20px" />}
+          onClick={() => navigate("/metrics")}
+        >
           Metrics
         </Button>
       </VStack>
