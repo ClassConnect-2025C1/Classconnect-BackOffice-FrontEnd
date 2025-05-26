aea1d7979774c22e481574f5a0191b71c462476f
franciscoL0pez <franlopez.2001@hotmail.com>
2025-05-25 23:51:36 -0300

    Prepare to deploy

--- backoffice/src/pages/Home.tsx
+++ backoffice/src/pages/Home.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Box, Heading, Text, VStack, HStack, Icon,Flex } from "@chakra-ui/react";
 import { MdDashboard, MdPeople, MdPersonAdd, MdBarChart } from "react-icons/md";
 
